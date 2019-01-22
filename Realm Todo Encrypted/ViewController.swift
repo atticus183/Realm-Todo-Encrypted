1b7b1fb7c489fc6237817de5b5f11fb03311fc34
Josh Rondestvedt <atticus183@gmail.com>
2019-01-21 22:06:22 -0600

    Initial Commit

---
+++ Realm Todo Encrypted/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Realm Todo Encrypted
+//
+//  Created by Josh R on 1/21/19.
+//  Copyright © 2019 Josh R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
